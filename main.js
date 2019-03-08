e70ad05d12f98caa41f51fdd0cd5e94d83fba7c6
franciscocaballero <caballerofrancisco22@yahoo.com>
2019-03-07 21:32:31 -0500

    added scoll function

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 window.odnAppId = '55f1d8f3a7f9095312620c51';
 window.odnApiKey = '5124f32117f91fb66299b9756991ce2b2c87d6fb';
+
+
+var scrollPositionY = window.scrollY;
+
+window.addEventListener('scroll',function() {
+    var navBar = document.getElementsByClassName("navigation")[0];
+   scrollPositionY = window.scrollY;
+    if (scrollPositionY >= 100) {
+        navBar.classList.add('sticky');
+    } else {
+        navBar.classList.remove('sticky');
+    }
+});
