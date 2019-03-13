d18ae08ae04dde19ed534bf1d12a2013a8a02bfa
franciscocaballero <caballerofrancisco22@yahoo.com>
2019-03-12 22:42:08 -0400

    adding fade in feature

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ window.odnApiKey = '5124f32117f91fb66299b9756991ce2b2c87d6fb';
 
 
 
+
+
+let firstHead = document.getElementById("first-head");
+console.log(firstHead.getBoundingClientRect());
+
+
+
 var scrollPositionY = window.scrollY;
 
 window.addEventListener('scroll',function() {
@@ -14,3 +21,37 @@ window.addEventListener('scroll',function() {
         navBar.classList.remove('sticky');
     }
 });
+
+
+
+
+
+// var animateHTML = function() {
+//     var elems;
+//     var windowHeight;
+//     function init() {
+//       elems = document.querySelectorAll('.hidden');
+//       windowHeight = window.innerHeight;
+//       addEventHandlers();
+//       checkPosition();
+//     }
+//     function addEventHandlers() {
+//       window.addEventListener('scroll', checkPosition);
+//       window.addEventListener('resize', init);
+//     }
+//     function checkPosition() {
+//       for (var i = 0; i < elems.length; i++) {
+//         var positionFromTop = elems[i].getBoundingClientRect().top;
+//         if (positionFromTop - windowHeight <= 0) {
+//           elems[i].className = elems[i].className.replace(
+//             'hidden',
+//             'fade-in-element'
+//           );
+//         }
+//       }
+//     }
+//     return {
+//       init: init
+//     };
+//   };
+//   animateHTML().init();
