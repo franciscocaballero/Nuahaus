10d6e03bc47b062c5564699a48f328d0676b2fc0
franciscocaballero <caballerofrancisco22@yahoo.com>
2019-03-27 21:27:36 -0400

    removed javascript responsive sunction

--- main.js
+++ main.js
@@ -46,18 +46,18 @@ window.addEventListener('scroll',function() {
 });
 
 
-function responsive(){
-    var navbr = document.querySelector(".navigation__list");
-    if (navbr.className === "topnav") {
-        navbr.className += " responsive";
-      } else {
-        x.className = "topnav";
-      }
-}
+// function responsive(){
+//     var navbr = document.querySelector(".navigation__list");
+//     if (navbr.className === "topnav") {
+//         navbr.className += " responsive";
+//       } else {
+//         x.className = "topnav";
+//       }
+// }
 
 
-var hamburger = document.querySelector(".navigation__link--icon");
+// var hamburger = document.querySelector(".navigation__link--icon");
 
-hamburger.addEventListener("click", function(){
-    console.log("red");
-})
+// hamburger.addEventListener("click", function(){
+//     console.log("red");
+// })
